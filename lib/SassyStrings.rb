46e44c6c8917968a78f50660e21f9fa8662bba0b
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-10-04 16:25:29 +0200

    Bumped 1.1.1

--- lib/SassyStrings.rb
+++ lib/SassyStrings.rb
@@ -4,8 +4,8 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register("SassyStrings", :path => "#{File.dirname(__FILE__)}/..")
 
 module SassyStrings
-  VERSION = "1.1.0"
-  DATE = "2014-09-28"
+  VERSION = "1.1.1"
+  DATE = "2014-10-04"
 end
 
 # Sassy String Functions
